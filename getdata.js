aef0b868d625a83f431d5bc9b0b9af24e125f808
wem5637 <wem5637@gmail.com>
2017-03-08 13:51:14 -0600

    Update getdata.js

--- getdata.js
+++ getdata.js
@@ -1,18 +1,42 @@
+var querystring = require('querystring');
+var http = require('http');
+var fs = require('fs');
+var bl = require('bl');
+var pages = 0;
+var results = [];
+var count = 0;
 
-  var querystring=require('querystring');
-  var http=require('http');
-  var fs = require('fs');
 
+var user = 'BoxerMc';
+http.get('http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&api_key=60e256b3eda3fe54934d79339614411f&user=' + user + '&format=json', function(res) {
+    var str = '';
+    console.log('Response is ' + res.statusCode);
+    
+    res.on('data', function(chunk) {
+        str += chunk;
+    });
 
-   http.get('http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&api_key=60e256b3eda3fe54934d79339614411f&user=BoxerMc&format=json', function(res){
-        var str = '';
-        console.log('Response is '+res.statusCode);
-        var fstream = fs.createWriteStream('cool.txt');
-        res.on('data', function (chunk) {
-               fstream.write(chunk);
-         });
+    res.on('end', function() {
+        str = JSON.parse(str);
+        pages = str.recenttracks["@attr"].totalPages;
 
-/*      res.on('end', function () {
-             console.log(str);
-        });*/
-  });
+        function getdata(index) {
+            http.get('http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&api_key=60e256b3eda3fe54934d79339614411f&user=' + user + '&page=' + (i + 1) + '&format=json', function(res) {
+                res.pipe(bl(function(err, data) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    results[index] = data.toString();
+                    count++;
+                    if (count === pages-1) {
+                      fs.writeFile('ok.txt', results);
+                    }
+                }));
+            });
+        }
+        for (var i = 0; i < pages; i++) {
+            getdata(i);
+        }
+
+    });
+});
